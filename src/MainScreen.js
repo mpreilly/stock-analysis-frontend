625d3abab09bca57ced0f7b123e9dd05acbedbc3
Alan Lai <AlanLai200@yahoo.com>
2019-06-28 13:56:52 -0700

    sider

--- src/MainScreen.js
+++ src/MainScreen.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import './MainScreen.css'
+import Slider from 'react-rangeslider'
+import 'react-rangeslider/lib/index.css'
 import ResultsPage from './components/ResultsPage'
 
 class MainScreen extends React.Component {
@@ -10,12 +12,19 @@ class MainScreen extends React.Component {
             symbol: '',
             sentimentPct: -1,
             technicalPct: -1,
+            value1: 50,
             listdata: [ {key: "BLK", value: "Blackrock"}, {key: "MSFT", value: "Microsoft"} ]
         };
 
         this.handleChange = this.handleChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
     }
+
+    handleChange2 = value1 => {
+      this.setState({
+        value1: value1
+      })
+    };
 
     handleChange(event) {
         this.setState({ symbolInput: event.target.value });
@@ -27,6 +36,11 @@ class MainScreen extends React.Component {
     }
 
     render() {
+      const test = {
+        0: 'High Sentiment',
+        50: 'Medium Sentiment and Technical',
+        100: 'High Technical'
+      }
         if (this.state.symbol === '') {
             return (
                 <form onSubmit={this.handleSubmit}>
@@ -44,23 +58,27 @@ class MainScreen extends React.Component {
                 </form>
             )
         } else if (this.state.sentimentPct === -1) {
+          const { value1 } = this.state
             return (
-                <div>
-                    <p>percentage picking bar goes here</p>
-                    <label for="customRange2">Example range</label>
-                    
-                    <form>
-                        <label>Sentiment</label>
-                        <input type="range" class="custom-range" min="0" max="100" step="5" id="customRange"></input>
-                        <label>Technical</label>
-                    </form>
-                </div>
+              <div className='slider'>
+                <Slider
+                  min={0}
+                  max={100}
+                  value={value1}
+                  step={1}
+                  labels={test}
+                  // onChangeStart={this.handleChangeStart}
+                  onChange={this.handleChange2}
+                  // onChangeComplete={this.handleChangeComplete}
+                />
+                <div className='value'>{value1}</div>
+              </div>
             )
         } else {
             return (
-                <ResultsPage 
-                    symbol={this.state.symbol} 
-                    techPct={this.state.technicalPct} 
+                <ResultsPage
+                    symbol={this.state.symbol}
+                    techPct={this.state.technicalPct}
                     sentPct={this.state.sentimentPct}
                 />
             )
